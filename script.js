0c7b4bba10acfadb33c851b1ddf4e08b77cc9044
VilleMansikkaniemi <73139444+VilleMansikkaniemi@users.noreply.github.com>
2024-02-07 04:41:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Your future JavaScript code will go here
+    document.getElementById('language-dropdown').addEventListener('change', function() {
+        var language = this.value;
+        // Perform action based on the selected language
+        // For example, redirect to a different language version of the site
+        if(language === 'en') {
+            window.location.href = '/html_en';
+        } else if(language === 'fi') {
+            window.location.href = '/html_fi';
+        } else if(language === 'sv') {
+            window.location.href = '/html_sv';
+        }
+    });
 });
