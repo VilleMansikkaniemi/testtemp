6ec3b6d0b9afc8b4ba61db3984b7002b69e17444
VilleMansikkaniemi <73139444+VilleMansikkaniemi@users.noreply.github.com>
2024-02-09 13:48:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Perform action based on the selected language
         // For example, redirect to a different language version of the site
         if(language === 'en') {
-            window.location.href = window.location.origin + "/testtemp/en_html"
+            window.location.href = window.location.origin + "/testtemp/html_en"
         } else if(language === 'fi') {
             window.location.href = window.location.origin + "/testtemp/html_fi";
         } else if(language === 'sv') {
